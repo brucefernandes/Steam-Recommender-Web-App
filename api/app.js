5ef7cd74fdef751ebc6c1e6cd5f2577bd3a108c2
Bruce Brad Fernandes <brucef300@gmail.com>
2022-08-16 19:02:23 -0400

    init api

--- api/app.js
+++ api/app.js
@@ -9,7 +9,13 @@ app.use(express.json())
 app.use(cors())
 app.use('/', gameRouter)
 
-mongoose.connect('mongodb+srv://bruce_fernandes:goanboy12345@videogamecluster.msextg4.mongodb.net/?retryWrites=true&w=majority');
+let mongoAtlasStr = 'mongodb+srv://bruce_fernandes:goanboy12345@videogamecluster.msextg4.mongodb.net/?retryWrites=true&w=majority'
+
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
+.then(()=>{
+    console.log("Connnected to Mongo Atlas");
+});
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
